3a3ca816b0ae1eef9843555344022f13aaea25c9
VernierNicolas <45039057+VernierNicolas@users.noreply.github.com>
2022-06-22 11:12:20 +0200

    Created Banner

---
+++ view-web-app/src/components/Banner.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="flex-container">
+    <div class="item">
+      <p>CesiEats</p>
+    </div>
+    <div class="item-x">
+      <button type="button" class="btn_sign_up" onclick="btn_sign_up">Connexion</button>
+      <button type="button" class="btn_sign_in" onclick="btn_sign_in">Inscription</button>
+    </div>
+  </div>
+
+</template>
+
+
+<style scoped>
+.item{
+  font-size: 24px;
+  margin-left: 20px;
+}
+.item-x{
+  margin-right: 20px;
+}
+.btn_sign_up{
+  margin-right: 20px;
+  background-color: #F6F6F6;
+  border-radius: 100px;
+  width: 126px;
+  height: 43px;
+}
+.btn_sign_in{
+  background-color: #F6F6F6;
+  border-radius: 100px;
+  width: 126px;
+  height: 43px;
+}
+
+</style>
--- view-web-app/src/views/HomeView.vue
+++ view-web-app/src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import TheWelcome from '@/components/TheWelcome.vue'
+import Banner from  '@/components/Banner.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <Banner/>
   </main>
 </template>
