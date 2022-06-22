73959609fcb6f9e945212cb38ea8474c47494929
AntoJvlt <antonin.jolivat@gmail.com>
2022-06-22 16:07:18 +0200

    Setup token models

---
+++ auth-microservice/src/model/AccessToken.ts
@@ -0,0 +1,35 @@
+import { DataTypes, InferAttributes, InferCreationAttributes, Model } from "sequelize";
+import sequelize from "../DBConnection";
+
+class AccessToken extends Model<InferAttributes<AccessToken>, InferCreationAttributes<Model>> {
+    declare id: number | undefined;
+    declare email: string;
+    declare accessToken: string;
+}
+
+AccessToken.init(
+    {
+        id: {
+            type: DataTypes.INTEGER.UNSIGNED,
+            autoIncrement: true,
+            primaryKey: true
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true
+        },
+        accessToken: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+    },
+    {
+        sequelize: sequelize,
+        tableName: "AccessTokens"
+    }
+);
+
+AccessToken.sync({ force: true });
+
+export default AccessToken;
---
+++ auth-microservice/src/model/RefreshToken.ts
@@ -0,0 +1,36 @@
+
+import { DataTypes, InferAttributes, InferCreationAttributes, Model } from "sequelize";
+import sequelize from "../DBConnection";
+
+class RefreshToken extends Model<InferAttributes<RefreshToken>, InferCreationAttributes<Model>> {
+    declare id: number | undefined;
+    declare email: string;
+    declare refreshToken: string;
+}
+
+RefreshToken.init(
+    {
+        id: {
+            type: DataTypes.INTEGER.UNSIGNED,
+            autoIncrement: true,
+            primaryKey: true
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true
+        },
+        refreshToken: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+    },
+    {
+        sequelize: sequelize,
+        tableName: "RefreshTokens"
+    }
+);
+
+RefreshToken.sync({ force: true });
+
+export default RefreshToken;
