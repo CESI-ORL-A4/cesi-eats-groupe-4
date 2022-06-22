52364a63960505ae37f87661963c3a4f84c88757
VernierNicolas <45039057+VernierNicolas@users.noreply.github.com>
2022-06-22 16:04:08 +0200

    Banner

--- view-web-app/src/components/Banner.vue
+++ view-web-app/src/components/Banner.vue
@@ -30,10 +30,13 @@ function homePage(){
 span{
   color: #ff5404
 }
-
+p{
+  margin: 5px;
+}
 .item{
-  font-size: 24px;
+  font-size: 40px;
   margin-left: 20px;
+  color: white;
 }
 .item-x{
   margin-right: 20px;
@@ -62,22 +65,5 @@ span{
 
 }
 
-.flex-container > div {
-  border-radius: 5px;
-  padding: 8px;
-}
 
-
-.item {
-  /* flex:0 1 auto; */
-  flex-shrink: 1;
-  align-self: center;
-  color: white;
-}
-
-.item-x {
-  /* flex:0 1 auto; */
-  align-self: center;
-  color: white;
-}
 </style>
